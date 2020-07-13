d1d3345ab49ffa08b88a0a776de0741ad4ed7f51
tako <tako_zhang@outlook.com>
2020-07-13 17:14:49 +0800

    first commit

--- src/main/java/com/takovh/testProject/codeEditor/CodeEditor.java
+++ src/main/java/com/takovh/testProject/codeEditor/CodeEditor.java
@@ -31,11 +31,8 @@ public class CodeEditor {
     private void run(){
         getFileList(new File(basePath));
         collectCode2File();
-        try {
-            deleteComments();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        deleteSingleLineComments();
+        deleteMultilineComments();
         deleteBlankLines();
     }
 
@@ -56,14 +53,18 @@ public class CodeEditor {
 
     /**
      * 初始化输出文件，若已存在则删除文件并重新创建
+     * @param file 需要初始化的文件
      */
-    private void initOutputFile(File outputFile) {
-        System.out.println("文件" + outputFile.getName() + "是否存在：" + outputFile.exists());
+    private void initOutputFile(File file) {
+//        System.out.println("文件" + file.getName() + "是否存在：" + file.exists());
+        Boolean flag = null;
         try {
-            if(outputFile.exists()){
-                System.out.println(outputFile.delete()?"删除成功！":"删除失败！");
+            if(file.exists()){
+                flag = file.delete();
+//                System.out.println(flag?"删除成功！":"删除失败！");
             }
-            System.out.println(outputFile.createNewFile()?"创建成功！":"创建失败！");
+            flag = file.createNewFile();
+//            System.out.println(flag?"创建成功！":"创建失败！");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -104,13 +105,11 @@ public class CodeEditor {
         }
     }
 
-
     /**
-     * 删除空行
+     * 删除满足正则表达式regexs的行
+     * @param regexs 正则表达式
      */
-    private void deleteBlankLines() {
-        String regex1 = "[\\s]*";
-        String regex2 = "[\\s]*";
+    private void deleteDesignatedLines(String...regexs){
         int i = 0;
         String tempPath = outputFile.getAbsolutePath() + ".temp";
         File tempFile = new File(tempPath);
@@ -123,7 +122,10 @@ public class CodeEditor {
             String tempString = null;
             // 以行为单位读取文件内容，一次读一整行
             while ((tempString = reader.readLine()) != null){
-                boolean flag = tempString.matches(regex1) | tempString.matches(regex2);
+                boolean flag = false;
+                for (String regex : regexs){
+                    flag = flag | tempString.matches(regex);
+                }
                 if(flag){
                     i++;
                     continue;
@@ -132,68 +134,86 @@ public class CodeEditor {
                 writer.write(System.getProperty("line.separator"));
                 writer.flush();
             }
-            System.out.println("删除空行:" + i + "行！");
+            System.out.println("删除了" + i + "行！");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             FileUtil.close(reader);
             FileUtil.close(writer);
         }
+        //删除原输出文件，将临时文件变为输出文件
+        if(outputFile.delete()) tempFile.renameTo(outputFile);
     }
 
     /**
-     * 删除注释
+     * 删除空行
      */
-    private void deleteComments() throws Exception {
-        FileReader fr = new FileReader("src\\qi_mo\\aa.java");
-        BufferedReader bufferedreader = new BufferedReader(fr);
+    private void deleteBlankLines() {
+        // ^\s*$ 匹配空行
+        String regex = "^\\s*$";
+        System.out.println("删除空行...");
+        deleteDesignatedLines(regex);
+    }
 
-        FileWriter fw = new FileWriter(new File("src\\qi_mo\\bb.java"));
-        BufferedWriter bw = new BufferedWriter(fw);
-        char ch;
-        String str = "";
-        int index;
-        boolean hasElseSign = false;
-        try{
-            while ((str = bufferedreader.readLine().trim()) != null) {
-                if (0 != str.length()) {
-                    if(hasElseSign == false){//如果没有多行注释
-                        for(index = 0;index < str.length();index++){
-                            ch = str.charAt(index);
-                            if((ch == '/')){
-                                if(str.charAt(index+1) == '/'){//是否有单行注释
-                                    str = str.substring(0,index);
-                                    break;
-                                }
-                                if(str.charAt(index+1)=='*'){//是否有多行注释
-                                    hasElseSign = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if(hasElseSign) continue;
-                    }
-                    else{//有多行注释时
-                        for(index = 0;index < str.length();index++){
-                            ch = str.charAt(index);
-                            if((ch == '*')&&(index<str.length()-1)&&(str.charAt(index+1) == '/')){
-                                hasElseSign = false;
-                                break;
-                            }
-                        }
-                        continue;
-                    }
-                    bw.write(str);//写入文件 把str存入缓冲
-                    bw.newLine();//换行
-                    bw.flush();//立即写入 （把缓冲里的所有内容写入）
-                    System.out.println(str);//
+    /**
+     * 删除单行注释
+     */
+    private void deleteSingleLineComments() {
+        // ^\s*//.*?$ 匹配单行注释
+        String regex = "^\\s*//.*?$";
+        System.out.println("删除单行注释...");
+        deleteDesignatedLines(regex);
+    }
 
+    private void deleteMultilineComments() {
+        System.out.println("删除多行注释...");
+        // ^\s*/\*.*$ 注释开始
+        String regex1 = "^\\s*/\\*.*$";
+        // ^.*\*/\s*$ 注释结束
+        String regex2 = "^.*\\*/\\s*$";
+        // 判断是否发现注释
+        boolean flag = false;
+        int i = 0;
+        String tempPath = outputFile.getAbsolutePath() + ".temp";
+        File tempFile = new File(tempPath);
+        initOutputFile(tempFile);
+        BufferedWriter writer = null;
+        BufferedReader reader = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(tempFile));
+            reader = new BufferedReader(new FileReader(outputFile));
+            String tempString = null;
+            // 以行为单位读取文件内容，一次读一整行
+            while ((tempString = reader.readLine()) != null){
+                // 1.判断是否进入多行注释
+                // 2.1若进入多行注释
+                if(flag){
+                    i++;
+                    // 发现多行注释结束标志
+                    if(tempString.matches(regex2)) flag = false;
+                    continue;
                 }
+                // 2.2若未进入多行注释
+                // 2.2.1发现多行注释开始标志
+                if(tempString.matches(regex1)){
+                    i++;
+                    flag = true;
+                    continue;
+                }
+                // 2.2.2未发现多行注释
+                writer.write(tempString);
+                writer.write(System.getProperty("line.separator"));
+                writer.flush();
             }
+            System.out.println("删除了" + i + "行！");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            FileUtil.close(reader);
+            FileUtil.close(writer);
         }
-        catch (Exception ioe){
-            ioe.printStackTrace();
-        }
+        //删除原输出文件，将临时文件变为输出文件
+        if(outputFile.delete()) tempFile.renameTo(outputFile);
     }
 
     public String getBasePath() {
@@ -215,7 +235,7 @@ public class CodeEditor {
     }
 
     public static void main(String[] args) {
-        String path = "C:\\Users\\tako_\\Desktop\\java";
+        String path = "C:\\Users\\tako_\\Desktop\\provider";
         CodeEditor codeEditor = new CodeEditor(path);
         System.out.println("BasePath:" + codeEditor.getBasePath());
         System.out.println("OutputPath:" + codeEditor.getOutputFile().getPath());
